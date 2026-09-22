ae705a3af88a13f470de5001b5e9ff9fe280416e
niovideoshelp-jpg <324197892+niovideoshelp-jpg@users.noreply.github.com>
2026-09-22 14:55:50 -0300

    fix: load bundled fonts through Remotion static asset paths

--- remotion-app/src/Root.tsx
+++ remotion-app/src/Root.tsx
@@ -1,5 +1,7 @@
+import './fonts';
 import './index.css';
 import {Composition} from 'remotion';
 import {Documentary} from './Documentary';
 import {DocumentaryMap} from './components/DocumentaryMap';
 export const RemotionRoot:React.FC=()=> <><Composition id="TyphoonVsRafaleIntro" component={Documentary} durationInFrames={5605} fps={30} width={1920} height={1080}/><Composition id="MapReview" component={DocumentaryMap} durationInFrames={330} fps={30} width={1920} height={1080} defaultProps={{highlights:[{country:'France',at:15}]}}/></>;
+
---
+++ remotion-app/src/fonts.ts
@@ -0,0 +1,8 @@
+import {staticFile, delayRender, continueRender, cancelRender} from 'remotion';
+if (typeof document !== 'undefined') {
+  const handle=delayRender('Bundled editorial fonts');
+  Promise.all([
+    new FontFace('Anton',`url("${staticFile('fonts/Anton-Regular.ttf')}")`).load(),
+    new FontFace('Oswald',`url("${staticFile('fonts/Oswald.ttf')}")`,{weight:'200 700'}).load(),
+  ]).then(fonts=>{fonts.forEach(font=>document.fonts.add(font));continueRender(handle);}).catch(cancelRender);
+}
